3f0616ee1fb0161a0ffd2bafb87c63cecfb575ec
Christoper <c.blake@hotmail.se>
2021-11-05 09:58:23 +0100

    15. Use the posted response to add to Blazor APP.

--- MachinePark/Pages/MachineOverView.cs
+++ MachinePark/Pages/MachineOverView.cs
@@ -1,10 +1,15 @@
 ﻿using Blazor.FlexGrid.DataAdapters;
 using MachinePark.Components;
+using MachinePark.Models;
 using MachinePark.Shared;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace MachinePark.Pages
@@ -14,6 +19,9 @@ namespace MachinePark.Pages
     {
         [Inject]
         public State State { get; set; }
+
+        //[Inject]
+        //public HttpClient httpClient { get; set; } //15 SLOW RAZOR UPDATES...
 
         protected AddMachineDialog AddMachineDialog { get; set; } //Points to the @ref in OverView.razor.
                                                                   //This is how Blazor gives access to components in my UI. @ref + Property in code behind.
@@ -27,11 +35,39 @@ namespace MachinePark.Pages
         {
             StateHasChanged();
         }
-        private MachinesList machines = new MachinesList();
+        public MachinesList Machines;
 
-        protected override async Task OnInitialedAsync()
+        protected override async Task OnInitializedAsync()
         {
-            machines = await 
+            Machines = await httpClient.GetFromJsonAsync<MachinesList>("api/machines"); //Table
+        }
+
+        private async Task AddMachine(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+            var machineDto = new CreateMachineDto { Name = name };
+            var response = await httpClient.PostAsJsonAsync<CreateMachineDto>("api/machines", machineDto);
+            //Response includes a body as well.
+            //if(res.IsSuccessStatusCode) THE IDEA
+            //{
+            //    Machines.Machines.Add(new Machine { Name = name });
+            //}
+            //15.
+            if(response.IsSuccessStatusCode)
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    using (var streamReader = new StreamReader(stream)) //System.Io
+                    {
+                        using (var jsonReader = new JsonTextReader(streamReader)) //Newtonsoft
+                        {
+                            var serializer = new Newtonsoft.Json.JsonSerializer();
+                            var machine = serializer.Deserialize<Machine>(jsonReader);
+                            Machines.Machines.Add(machine);
+                        }
+                    }
+                }
+            }
         }
 
         //public List<Machine> Machines { get; set; }
